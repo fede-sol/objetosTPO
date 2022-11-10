5f13d4ba48d880a08b07a21b0f08ee32dadd39e3
fede-sol <fedesolanes1@gmail.com>
2022-11-09 21:41:40 -0300

    Creada sección del catalogo
    Se crearon las clases para los objetos de cada producto y del catalogo
    en sí.
    
    Además creé el template para la clase administradora del catalogo.
    
    Todavía falta terminar las vista del catalogo

---
+++ src/inventario/AdministradorCatalogo.java
@@ -0,0 +1,47 @@
+package inventario;
+
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ * @author Fede
+ */
+public class AdministradorCatalogo {
+    
+     private Catalogo catalogo;
+
+    public Catalogo getCatalogo() {
+        return catalogo;
+    }
+    
+    
+public void guardarProducto(Producto p){
+    
+    
+}
+    
+public void removerProducto(int cod){
+    
+}
+
+public void guardarModificaciones(int cod, String descripcion, int precioUnitario, int stock, int stockMin){
+    
+}
+
+public void actualizarStock(int cod, int cantVendida){
+    
+}
+
+public ArrayList<Producto> getProductosStockMin(){
+    
+    return null;
+}
+
+     
+     
+     
+     
+    
+}
--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -7,6 +7,60 @@ package inventario;
 public class Producto {
     
     
+    private int codigo;
+    private int precioUnitario;
+    private int stock;
+    private int stockMinimo;
+    private String descripcion;
+
+    public Producto(int codigo, int precioUnitario, int stock, int stockUnitario, int stockMinimo,String descripcion){
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precioUnitario = precioUnitario;
+        this.stock = stock;
+        this.stockMinimo = stockMinimo;
+        
+    }
     
+    
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setPrecioUnitario(int precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setStockMinimo(int stockMinimo) {
+        this.stockMinimo = stockMinimo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public int getStockMinimo() {
+        return stockMinimo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
     
 }
