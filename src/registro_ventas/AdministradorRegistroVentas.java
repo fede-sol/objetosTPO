ef8e9689ec7a4293e7407a1ff2240dfa0f37225c
fede-sol <fedesolanes1@gmail.com>
2022-11-12 13:06:35 -0300

    Separé la seccion de registro de ventas de la de ventas

--- src/registro_ventas/AdministradorRegistroVentas.java
+++ src/registro_ventas/AdministradorRegistroVentas.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ventas;
+package registro_ventas;
 import java.util.ArrayList;
+import ventas.Venta;
 /**
  *
  * @author Tati
