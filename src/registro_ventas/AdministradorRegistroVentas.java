57de33d46ff71f0c6d67280f9343f62ddca1c1f2
tatlopez <tatianalopez0727@gmail.com>
2022-11-13 20:02:15 -0300

    :(

--- src/registro_ventas/AdministradorRegistroVentas.java
+++ src/registro_ventas/AdministradorRegistroVentas.java
@@ -3,8 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package registro_ventas;
-import java.util.ArrayList;
 import ventas.Venta;
+import java.util.ArrayList;
+import inventario.AdministradorCatalogo;
+import inventario.Catalogo;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utilidades.FileHandler;
 /**
  *
  * @author Tati
@@ -12,6 +18,9 @@ import ventas.Venta;
 public class AdministradorRegistroVentas {
     
     private RegistroVentas registroVentas;
+    private AdministradorCatalogo adminCatalogo;
+    private FileHandler fileHandler = new FileHandler();
+    private final File RUTA_INVENTARIO = new File("src/saves/registroVentas.dat");
     
     
     public AdministradorRegistroVentas() {
@@ -20,17 +29,32 @@ public class AdministradorRegistroVentas {
     }
     
     private void importarRegistroVentas(){
-        registroVentas = new RegistroVentas();
+        if (RUTA_INVENTARIO.exists()){
+            try {
+                registroVentas = (RegistroVentas) fileHandler.importarObjeto(RUTA_INVENTARIO);
+            } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AdministradorRegistroVentas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public void exportarRegistroVentas(){
+        
+        fileHandler.exportarObjeto(registroVentas, RUTA_INVENTARIO);
     }
     
     public void agregarVenta(Venta v) {
         registroVentas.getListaVentas().add(v);
         registroVentas.setCantidadVentas(registroVentas.getCantidadVentas()+ 1);
+        exportarRegistroVentas();
         
     }
     
     public void getProductosMasVendidos() {
-        
+        int cantVentas = registroVentas.getCantidadVentas();
+        ArrayList ventas = registroVentas.getListaVentas();
+        ArrayList productos = adminCatalogo.getCatalogo().getListaProducto();
+        ArrayList productosMasVendidos = new ArrayList();  
     }
     
     
